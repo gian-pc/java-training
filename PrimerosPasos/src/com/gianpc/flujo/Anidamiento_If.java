c220069dfdb4fde0fcd0ca638d152a8d76e6b34f
Gian Carlos Paucar Cortez <77996469+gian-pc@users.noreply.github.com>
2026-03-13 15:57:38 -0500

    refactor: extrae validación de mayoría de edad a variable booleana

--- PrimerosPasos/src/com/gianpc/flujo/Anidamiento_If.java
+++ PrimerosPasos/src/com/gianpc/flujo/Anidamiento_If.java
@@ -8,16 +8,23 @@ public class Anidamiento_If {
 		// TODO Auto-generated method stub
 
 		int edad = Integer.parseInt(JOptionPane.showInputDialog("Introduce tu edad, por favor: "));
+		
+		boolean mayor_edad = false;
+		
+		if(edad >= 18) {
+			
+			mayor_edad = true;
+		}
 
-		if (edad >= 18) {
+		if (mayor_edad) {
+			
 			String carnet = JOptionPane.showInputDialog("Tienes carnet?: ");
 
-			if (carnet.equalsIgnoreCase("si")) JOptionPane.showMessageDialog(null, "Puedes comprar el coche");
+			if (carnet.equalsIgnoreCase("si"))JOptionPane.showMessageDialog(null, "Puedes comprar el coche");
 
-			 else JOptionPane.showMessageDialog(null, "Sin carnet no hay coche");
-			
+			else JOptionPane.showMessageDialog(null, "Sin carnet no hay coche");
+
 		} else JOptionPane.showMessageDialog(null, "Si ere menor no puedes tener carnet y por lo tanto tampoco coche");
-		
 
 	}
 
